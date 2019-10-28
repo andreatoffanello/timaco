7277bb3f2a1135c093742401c16f6bee5e5aa136
Andrea Toffanello <andrea.toffanello@gmail.com>
2019-10-28 11:18:41 +0100

    Initial commit

---
+++ docs/gulpfile.js
@@ -0,0 +1,100 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    rename      = require('gulp-rename'),
+    cssmin      = require('gulp-cssnano'),
+    prefix      = require('gulp-autoprefixer'),
+    plumber     = require('gulp-plumber'),
+    notify      = require('gulp-notify'),
+    sassLint    = require('gulp-sass-lint'),
+    sourcemaps  = require('gulp-sourcemaps');
+    // Temporary solution until gulp 4
+    // https://github.com/gulpjs/gulp/issues/355
+    runSequence = require('run-sequence');
+    livereload  = require('gulp-livereload');
+
+var displayError = function(error) {
+  // Initial building up of the error
+  var errorString = '[' + error.plugin.error.bold + ']';
+  errorString += ' ' + error.message.replace("\n",''); // Removes new line at the end
+
+  // If the error contains the filename or line number add it to the string
+  if(error.fileName)
+      errorString += ' in ' + error.fileName;
+
+  if(error.lineNumber)
+      errorString += ' on line ' + error.lineNumber.bold;
+
+  // This will output an error like the following:
+  // [gulp-sass] error message in file_name on line 1
+  console.error(errorString);
+};
+
+var onError = function(err) {
+  notify.onError({
+    title:    "Gulp",
+    subtitle: "Failure!",
+    message:  "Error: <%= error.message %>",
+    sound:    "Basso"
+  })(err);
+  this.emit('end');
+};
+
+var sassOptions = {
+  outputStyle: 'expanded'
+};
+
+var prefixerOptions = {
+  // browsers: ['last 2 versions'],
+
+  overrideBrowserslist: [
+    'last 10 version',
+    '> 1%',
+    'IE 8'
+  ]
+};
+
+// BUILD SUBTASKS
+// ---------------
+
+
+gulp.task('styles', function() {
+  return gulp.src('scss/style.scss')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions))
+    .pipe(prefix(prefixerOptions))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('css'))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('css'))
+    .pipe(livereload());
+});
+
+gulp.task('sass-lint', function() {
+  gulp.src('scss/*.scss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('scss/*.scss', gulp.series('styles'));
+  gulp.watch('scss/*/*.scss', gulp.series('styles'));
+});
+
+// BUILD TASKS
+// ------------
+
+gulp.task('scss', gulp.series('styles', 'watch', function (done) {
+    done();
+}));
+
+//gulp.task('default', function(done) {
+//  runSequence('styles', 'watch', done);
+//});
+//
+//gulp.task('build', function(done) {
+//  runSequence('styles', done);
+//});
---
+++ docs/js/main.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+
+  const players = Array.from(document.querySelectorAll('.js-player')).map(p => new Plyr(p));
+
+
+  $(".loader").removeClass("on");
+
+  
+
+
+  // CURSOR
+
+  function cursor() {
+
+  	document.body.style.cursor = 'none';
+  	/* why we do this here and not in css?
+  	 * If the user dosn't have javascript onboard he
+  	 * don't see any cursor by default and that's shit
+  	 */
+
+  	var cursor = document.createElement( 'div' );
+  	cursor.classList.add( 'cursor' );
+  	document.body.appendChild( cursor );
+
+  	var follow = document.createElement( 'div' );
+  	follow.classList.add( 'follow' );
+  	document.body.appendChild( follow );
+
+  	function move( obj, event ) {
+  	    obj.style = '';
+  	    obj.style.top = event.clientY + 'px';
+  	    obj.style.left = event.clientX + 'px';
+  	}
+
+  	if ( cursor ) {
+  	    window.addEventListener( 'mousemove', function( event ) {
+
+  	        // legend ;)
+  	        var e = event;
+  	        var t = e.target;
+  	        var f = follow;
+  	        var c = cursor;
+
+  	        // rambazamba
+  	        if ( (t == $('.link')) || (t == $('p a')) ) {
+  	            // c.style.backgroundColor = 'transparent';
+
+  	            // f.style.top = t.offsetTop + 'px';
+  	            // f.style.left = t.offsetLeft + 'px';
+  	            // f.style.width = t.clientWidth + 'px';
+  	            // f.style.height = t.clientHeight + 'px';
+
+  	            // if ( t.classList.contains( 'button--round' ) )
+  	            //     f.style.borderRadius = '50%';
+
+  	            var aEl = $(t)
+
+  				var offset = $(aEl).offset();
+  				var width = $(aEl).width();
+  				var height = $(aEl).height();
+
+  				var centerX = offset.left + width / 2;
+  				var centerY = offset.top + height / 2;
+
+  	            $( f, c ).css({
+  	            	'top' : centerY,
+  	            	'left' : centerX
+  	            });
+
+
+  	            $( f, c ).addClass( 'on_hover' );
+
+  	        }
+
+
+  	        //if ( t.tagName == 'A' )
+
+
+  	        else {
+  	            move( c, e );
+  	            move( f, e );
+  	            //$(f).removeClass( 'on_hover' );
+  	        }
+  	    });
+  	}
+
+  	$('a, .grid_item').mouseenter( function() {
+  		$( '.follow').addClass( 'on_hover' );
+  		$( '.cursor').addClass( 'on_hover' );
+  	});
+
+  	$('a, .grid_item').mouseleave( function() {
+  		$( '.cursor').removeClass( 'on_hover' );
+  		$( '.follow').removeClass( 'on_hover' );
+  	});
+
+  	$(document).on('mousemove', function(e){
+  	    $(cursor).css({
+  	        left:  e.pageX,
+  	        top:   e.pageY - $('body').scrollTop()
+  	    });
+  	});
+
+  };
+
+  cursor();
+
+
+});
